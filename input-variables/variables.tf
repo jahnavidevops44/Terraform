68e4b5fbf5a128b88c069515f3765a88ef3f4fde
jahnavidevops44 <146941093+jahnavidevops44@users.noreply.github.com>
2023-10-10 16:42:30 +0530

    Create variables.tf
    for main.tf we were passing variable default values

---
+++ input-variables/variables.tf
@@ -0,0 +1,19 @@
+variable "rgname"{
+  type= string
+  description = "used for naming resource group"
+  default = "myTFjdrg"
+}
+variable "rglocation"{
+  default = "East US"
+}
+
+variable "vnetname"{
+  default = "myTFvnet"
+}
+variable "subnetname"{
+  default = "myTFjdsubnet"
+}
+
+
+
+
