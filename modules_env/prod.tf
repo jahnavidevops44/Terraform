ac44691e094e039097d0b64e18178ef9a6ed69c0
jahnavidevops44 <146941093+jahnavidevops44@users.noreply.github.com>
2023-10-18 15:28:57 +0530

    Create prod.tf
    prod was saved

---
+++ modules_env/prod.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    } 
+  }
+}
+module "module_prod" {
+  source = "./modules"
+    prefix = "prod"
+    vnet_cidr_prefix = "10.30.0.0/16"
+    subnet_cidr_prfix = "10.30.1.0/24"
+    rgname = "prod_reg"
+    subnet = "prod_subnet"
+}
