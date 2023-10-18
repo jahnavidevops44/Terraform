2765ed666d539fbcee90b5079ce24e58f768c60d
jahnavidevops44 <146941093+jahnavidevops44@users.noreply.github.com>
2023-10-18 15:26:57 +0530

    Create dev.tf
    development env set

---
+++ modules_env/dev.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    } 
+  }
+}
+module "module_dev" {
+  source = "./modules"
+    prefix = "dev"
+    vnet_cidr_prefix = "10.20.0.0/16"
+    subnet_cidr_prfix = "10.20.1.0/24"
+    rgname = "dev_reg"
+    subnet = "dev_subnet"
+}
+
