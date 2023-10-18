56a528bc3a6eaaad647e879be4f90f22f28f21d9
jahnavidevops44 <146941093+jahnavidevops44@users.noreply.github.com>
2023-10-18 15:36:41 +0530

    Update main.tf
    address space changes

--- modules_env/modules/main.tf
+++ modules_env/modules/main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
+
 provider "azurerm" {
   
 features {}
@@ -23,12 +16,12 @@ resource "azurerm_virtual_network" "vnet1" {
    name                = "jdvnet"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
-  address_space       = ["10.0.0.0/16"]
+  address_space      =  azurerm_virtual_network.vnet1.address_space
 
 }
 resource "azurerm_subnet" "subnet1" {
   name                 = "jdsubnet"
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_space     = azurerm_virtual_network.subnet1.address_space 
 }
