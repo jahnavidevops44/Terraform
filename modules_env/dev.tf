3ececa4ac2fd74d43ba271cd3dc535a85eccf5e5
jahnavidevops44 <146941093+jahnavidevops44@users.noreply.github.com>
2023-10-19 11:45:58 +0530

    Update dev.tf
    cleared errors

--- modules_env/dev.tf
+++ modules_env/dev.tf
@@ -7,11 +7,12 @@ terraform {
   }
 }
 module "module_dev" {
-  source = "./modules"
+    source = "./modules"
     prefix = "dev"
     vnet_cidr_prefix = "10.20.0.0/16"
-    subnet_cidr_prfix = "10.20.1.0/24"
-    rgname = "dev_reg"
-    subnet = "dev_subnet"
+    subnet_cidr_prefix = "10.20.1.0/24"
+    rgname = "DevRg"
+    subnetname = "DevSubnet"
+    #vnetname = "DevVnet"
 }
 
